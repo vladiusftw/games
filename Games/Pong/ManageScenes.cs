4e3eebd5ec1c56c87d4c55202d8c21ac8375f3fe
vladiusftw <61135058+vladiusftw@users.noreply.github.com>
2021-10-27 17:49:38 +0400

    Add files via upload

---
+++ Games/Pong/ManageScenes.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour
+{
+    
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void MainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ChoosePlayerScene()
+    {
+        SceneManager.LoadScene("ChoosePlayers");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name=="StartScreen")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("ChoosePlayers");
+            }
+                
+
+
+
+        }
+    }
+
+}
